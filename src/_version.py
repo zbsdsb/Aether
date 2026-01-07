c6e1cdbb3c9bb9ced56e947f17363a1425160033
fawney19 <elky0401@gmail.com>
2026-01-07 20:19:49 +0800

    chore: bump version to 0.2.5

--- src/_version.py
+++ src/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.2.4'
-__version_tuple__ = version_tuple = (0, 2, 4)
+__version__ = version = '0.2.5'
+__version_tuple__ = version_tuple = (0, 2, 5)
 
 __commit_id__ = commit_id = None
